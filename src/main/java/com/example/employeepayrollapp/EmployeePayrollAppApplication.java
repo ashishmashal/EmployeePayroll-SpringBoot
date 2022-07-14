e8f99bafe1735476cc7042c1461cb30d5c22efd5
ashishmashal <ashishmashal5@gmail.com>
2022-07-14 15:33:00 +0530

    Added: Using Sl4J Printed The Welcome To Emplpoyee Payroll App.

--- src/main/java/com/example/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/example/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -1,14 +1,15 @@
 package com.example.employeepayrollapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@Slf4j
 @SpringBootApplication
 public class EmployeePayrollAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EmployeePayrollAppApplication.class, args);
-        System.out.println("Welcome To Emplpoyee Payroll App");
+        log.info("Welcome To Emplpoyee Payroll App");
     }
 
 }
